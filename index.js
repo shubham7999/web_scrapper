c6b92b5cfe2ffe3384be6f8cd7bf64acf2799379
Shubham <shubhampokhriyal2018@gmail.com>
2021-06-16 14:06:14 +0530

    completed extract

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/chennai-super-kings-vs-kings-xi-punjab-53rd-match-1216506/ball-by-ball-commentar";
+const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/chennai-super-kings-vs-kings-xi-punjab-53rd-match-1216506/ball-by-ball-commentary";
 const request = require("request");
+const cheerio = require("cheerio");
 console.log("before");
 request(url , callback);
 function callback(err, response, html)
@@ -9,8 +10,33 @@ function callback(err, response, html)
        console.log("Something went wrong");
    }
    else{
-       console.log(html);
+       extract(html);
        console.log("No error");
    }
 }
-console.log("After");
+
+function extract(html)
+{
+    let $ = cheerio.load(html);
+    let ele = $(".d-flex.match-comment-padder.align-items-center>div>div>div>div>p");
+    let val = $(ele[0]).text();
+    let htmldoc = $(ele[0]).html();
+    console.log("html" , htmldoc);
+    console.log(val);
+    console.log($(ele[0]).html());
+   console.log(ele);
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// .d-flex.match-comment-padder.align-items-center>div>div>div>div>p
